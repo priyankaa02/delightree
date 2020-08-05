27b92b05b6dd79c2ad66957bc5ed879f65d1308b
priyanka <priyankaa0207@gmail.com>
2020-08-05 23:48:31 +0530

    commit change of header

--- src/components/ReviewTask/Header.js
+++ src/components/ReviewTask/Header.js
@@ -1,39 +1,39 @@
 import React from 'react';
-import {StyleSheet, Text, View, ImageBackground} from 'react-native';
-import {Icon} from 'react-native-elements';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import {dimensions, colors} from '../../styles/variables';
 import {responsive} from '../../styles/mixins';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const Header = (props) => {
   return (
-    <ImageBackground
-      style={styles.header}
-      source={require('../../images/adminBanner.png')}
-      resizeMode="stretch">
-      <View style={styles.headerItem}>
-        <Icon name="home" color="white" size={30} />
-        <Text style={styles.headerTitle}>{props.title}</Text>
-        <Icon name="notifications" color="white" size={30} />
-      </View>
-    </ImageBackground>
+    <View style={styles.header}>
+      <TouchableOpacity onPress={props.onClickBack}>
+        <Icon
+          style={{marginTop: responsive.vertical(3)}}
+          name="ios-chevron-back"
+          color={colors.white}
+          size={25}
+        />
+      </TouchableOpacity>
+      <Text style={styles.headerTitle}>{props.title}</Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   header: {
     width: dimensions.width,
-    height: responsive.vertical(130),
-    paddingTop: 10,
-    // backgroundColor: colors.btn_blue,
+    height: responsive.vertical(90),
+    paddingTop: 20,
+    // flex: 1,
+    flexDirection: 'row',
+    paddingLeft: responsive.horizontal(30),
+    backgroundColor: colors.purple,
   },
   headerTitle: {
     color: 'white',
-    fontSize: 25,
-    fontFamily: 'Lobster',
-  },
-  headerItem: {
-    padding: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    fontWeight: 'bold',
+    fontSize: 22,
+    marginLeft: responsive.horizontal(30),
   },
 });
